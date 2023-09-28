e1b362387ae790742c519a76fc8304b0d9be1c5d
BlackKnight683 <contact@aayush.social>
2023-09-28 15:18:53 +0530

    login system

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("header",header, name="header"),
     path("home",home,name="home"),
     path("about", about, name="about"),
+    path("contact", contact, name="contact"),
+    path("register", register, name="register"),
+    path("login_post", login_post, name="login_post"),
+    path("register_post", register_post, name="register_post"),
 ]
