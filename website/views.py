2e5e0077f50a9555a6e8ff146b17033f403d456e
om-aroraa <120252758+om-aroraa@users.noreply.github.com>
2023-09-28 15:22:10 +0530

    login

--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from users.models import User
 # Create your views here.
 
 def login(request):
-    return render(request, "login.html")
+    return render(request, "login.html", {"error": ""})
 
 def header(request):
     return render(request,"header.html")
@@ -19,3 +20,32 @@ def contact(request):
 
 def register(request):
     return render(request,"register.html")
+
+# make a post request handler for login
+# make a post request handler for register
+
+
+def login_post(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    if (username == None or password == None):
+        return render(request, "login.html", {"error": "Invalid username or password"})
+    # check if username and password are in database
+    user = User.objects.filter(username=username, password=password)
+    if user:
+        return render(request, "home.html")
+    else:
+        return render(request, "login.html", {"error": "Invalid username or password"})
+    
+def register_post(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    email = request.POST.get("email")
+    # check if username and password are in database
+    user = User.objects.filter(username=username)
+    if user:
+        return render(request, "register.html", {"error": "Username already exists"})
+    else:
+        user = User(username=username, password=password, email=email)
+        user.save()
+        return render(request, "login.html", {"error": "Account created successfully. Please login."})
