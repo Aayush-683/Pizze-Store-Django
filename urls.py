78e6d7de46b78eab0ae8082229c95774fd80b98a
BlackKnight683 <contact@aayush.social>
2023-09-28 14:38:42 +0530

    fixing files

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("", login, name="login"),
-    path("header",header, name="header"),
-    path("home",home,name="home"),
-    path("about", about, name="about"),
-]
